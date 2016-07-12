e2806b6c1aaf698bb4116eab2139c8f4ea261120
Iván Stepaniuk <istepaniuk@gmail.com>
2016-07-12 16:52:52 +0200

    updated the update script

--- update.sh
+++ update.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-ssh blog.istepaniuk.com "cd istepaniuk-com; git pull && git submodule init && git submodule update && jekyll b && find /var/www/blog -name \"*.html\" -exec tidy -config _tidy_config.txt {} \\;"
+ssh blog.istepaniuk.com "cd istepaniuk-com; git pull && git submodule init && git submodule update && jekyll build && find /var/www/blog -name \"*.html\" -exec tidy -config _tidy_config.txt {} \\;"
